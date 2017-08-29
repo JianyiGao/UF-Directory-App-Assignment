628ce9ece3c6965ced74042c1d73d7bdc36b11bd
JianyiGao <gaojianyi912@gmail.com>
2017-08-28 22:54:56 -0400

    Searching method changed, style added

--- listingController.js
+++ listingController.js
@@ -9,18 +9,29 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
      */
      // To do: Showing new listing in listings in order by code/name
     $scope.addListing = function() {
-      Listings.push({
-        code: $scope.newCode,
-        name: $scope.newBuildingName,
-      });
+      let newListing = new Object();
+      newListing.code = $scope.newCode;
+      newListing.name = $scope.newBuildingName;
+      newListing.coordinates = new Object();
+      newListing.coordinates.latitude = $scope.newLatitude;
+      newListing.coordinates.longitude = $scope.newLongitude;
+      newListing.address = $scope.newAddress;
+
+      Listings.push(newListing);
+
       // Clear input fields after push
       $scope.newCode = "";
       $scope.newBuildingName = "";
+      $scope.newLatitude = "";
+      $scope.newLongitude = "";
+      $scope.newAddress = "";
     };
-    $scope.deleteListing = function(index) {
+    $scope.deleteListing = function(item) {
+      var index = Listings.indexOf(item);
       Listings.splice(index, 1);
     };
-    $scope.showDetails = function(index) {
+    $scope.showDetails = function(item) {
+      let index = Listings.indexOf(item);
       var thisListing = Listings[index];
       $scope.code = thisListing.code;
       $scope.name = thisListing.name;
